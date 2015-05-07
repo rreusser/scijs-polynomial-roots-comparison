665feb58b64c719cea0a4761f1beb828c95cd4eb
Ricky Reusser <rreusser@users.noreply.github.com>
2015-05-07 01:18:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ for(var i=0; i<roots.length; ++i) {
 }
 
 
-// Search epected real/imag for observed real/imag pairs:
+// Greedy search search for the root they're supposed to be the error of:
 function error( er, ei, or, oi ) {
   var hits = [];
   var i,j,idx,errmin,total = 0;
